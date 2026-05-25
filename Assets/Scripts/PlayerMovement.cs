9c66570fee65af2aab3ea70ddf95ce91f93a7a1e
ArielEskin <arieleskin360@gmail.com>
2026-05-25 18:17:41 +0300

    Fixed sideways movement bug and error

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,52 +2,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    [SerializeField] float xPos;
-    [SerializeField] float yPos;
-    [SerializeField] float zPos;
-    [SerializeField] int trackNumber = 0;
+    [SerializeField] float moveSpeed = 5f;
     [SerializeField] private int sideSpeed = 9;
+    
+    [SerializeField] int trackNumber = 0; // Tracks target X position (-1, 0, 1)
     [SerializeField] private bool isMoving;
-    [SerializeField] int moveDirection; //  (1=Left) (2=Right)
+    [SerializeField] int moveDirection; // (1=Left) (2=Right)
     
     void Update()
     {
-        transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime),Space.World);
-        xPos = gameObject.transform.position.x;
-        zPos = gameObject.transform.position.z;
+        // Constant forward movement
+        transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime), Space.World);
 
-        if (isMoving && moveDirection == 1) // Moving Left
+        if (isMoving) // 2. Handle horizontal movement
         {
-            transform.Translate(Vector3.left * (sideSpeed * Time.deltaTime),Space.World);
-            if (xPos <= trackNumber)
+            if (moveDirection == 1) // Moving Left
             {
-                isMoving = false;
-                moveDirection = 0;
-                transform.position = new Vector3(trackNumber, yPos, zPos);
+                transform.Translate(Vector3.left * (sideSpeed * Time.deltaTime), Space.World);
+
+                if (transform.position.x <= trackNumber)
+                {
+                    StopMoving();
+                }
+            }
+            else if (moveDirection == 2) // Moving Right
+            {
+                transform.Translate(Vector3.right * (sideSpeed * Time.deltaTime), Space.World);
+
+                if (transform.position.x >= trackNumber)
+                {
+                    StopMoving();
+                }
             }
         }
-        if (isMoving && moveDirection == 2) // Moving Right
-        {
-            transform.Translate(Vector3.left * (sideSpeed * Time.deltaTime * -1),Space.World);
-            if (xPos >= trackNumber)
-            {
-                isMoving = false;
-                moveDirection = 0;
-                transform.position = new Vector3(trackNumber, yPos, zPos);
-            }
-        }
+    }
+
+    private void StopMoving() // Helper method to cleanly snap and stop
+    {
+        isMoving = false;
+        moveDirection = 0;
+
+        transform.position = new Vector3(trackNumber, transform.position.y, transform.position.z);
     }
     
     public void LeftMove()
     {
+        if (isMoving) return; // Prevent pressing again mid-move
+
         if (trackNumber == 0)
         {
             isMoving = true;
             moveDirection = 1;
             trackNumber = -1;
         }
-        if (trackNumber == 1)
+        else if (trackNumber == 1)
         {
             isMoving = true;
             moveDirection = 1;
@@ -57,13 +65,15 @@ public class PlayerMovement : MonoBehaviour
 
     public void RightMove()
     {
+        if (isMoving) return; // Prevent pressing again mid-move
+
         if (trackNumber == 0)
         {
             isMoving = true;
             moveDirection = 2;
             trackNumber = 1;
         }
-        if (trackNumber == -1)
+        else if (trackNumber == -1)
         {
             isMoving = true;
             moveDirection = 2;
