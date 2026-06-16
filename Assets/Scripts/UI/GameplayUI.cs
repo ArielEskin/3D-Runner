2ac75a908cce809a71f18bd50c850de26317f29a
MaximAbramov <maximab2002@gmail.com>
2026-06-16 23:13:39 +0300

    Patch UI
    added more UI update the sounds settings ui
    in game new coin UI

--- Assets/Scripts/UI/GameplayUI.cs
+++ Assets/Scripts/UI/GameplayUI.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using UnityEngine.UIElements;
-
+using UnityEngine.UI;
 
 public class GameplayUI : MonoBehaviour
 {
     [SerializeField] private GameObject buttonControlsPanel;
-
+    [SerializeField] private Image buttonModeImage;
+    [SerializeField] private Image touchModeImage;
     
     
     // Update is called once per frame
@@ -14,14 +14,26 @@ public class GameplayUI : MonoBehaviour
         if (InputManager.instance.currentMode == InputMode.Buttons)
         {
             buttonControlsPanel.SetActive(true);
-
+            
+            SetAlpha(buttonModeImage, 0.5f); // Active
+            SetAlpha(touchModeImage, 1f);    // Inactive
 
         }
         else
         {
             buttonControlsPanel.SetActive(false);
 
-            
+            SetAlpha(buttonModeImage, 1f);   // Inactive
+            SetAlpha(touchModeImage, 0.5f);  // Active
         }
+        
     }
+    
+    private void SetAlpha(Image image, float alpha)
+    {
+        Color c = image.color;
+        c.a = alpha;
+        image.color = c;
+    }
+    
 }
